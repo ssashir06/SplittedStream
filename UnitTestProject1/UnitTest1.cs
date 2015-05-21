591c8594d075bdadf10c904de95149cf0850b3d4
Hiroshi Takeda <takejaponpon+git@gmail.com>
2015-05-22 03:11:06 +0900

    7z file test is added

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SplittedStream;
 using System.IO;
 using System.Text;
+using System.Linq;
 
 namespace UnitTestProject1
 {
@@ -10,15 +11,47 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod_2ファイル()
         {
-            var files = new[]{@"..\..\TestFiles\File1.txt", @"..\..\TestFiles\File2.txt"};
+            var files = new[] { @"TestFiles\File1.txt", @"TestFiles\File2.txt" };
             using (var splittedStreamReadFiles = new SplittedStreamReadFiles(files))
             using (var textReader = new StreamReader(splittedStreamReadFiles, Encoding.UTF8))
             {
                 var text = textReader.ReadToEnd();
                 Console.WriteLine(text);
+
+                Assert.AreEqual("this is a file 1.this is a file 2.", text);
             }
         }
+
+        [TestMethod]
+        public void TestMethod_7zファイル一覧()
+        {
+            var sz = @"TestFiles\sample1.7z";
+
+            string fileNamesCorrect;
+            string fileNamesTest;
+
+            using (var fs = new FileStream(sz, FileMode.Open, FileAccess.Read))
+            using (var sevenzip = new SevenZip.SevenZipExtractor(fs))
+            {
+                var files = sevenzip.ArchiveFileNames;
+                fileNamesCorrect = string.Join(Environment.NewLine, files);
+                Console.WriteLine(fileNamesCorrect);
+            }
+
+            var splitedFiles = Directory.EnumerateFiles(@"TestFiles\splittedfiles_sample1.7z").ToArray();
+            Array.Sort(splitedFiles);
+            using (var fs = new SplittedStreamReadFiles(splitedFiles))
+            using (var sevenzip = new SevenZip.SevenZipExtractor(fs))
+            {
+                var files = sevenzip.ArchiveFileNames;
+                fileNamesTest = string.Join(Environment.NewLine, files);
+                Console.WriteLine(fileNamesTest);
+            }
+
+            Assert.AreEqual(fileNamesCorrect, fileNamesTest);
+        }
+
     }
 }
